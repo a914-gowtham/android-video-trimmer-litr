c88b41cf34dc2bfd5032bfed200bc175040b9468
gowtham <a914gowtham@gmail.com>
2021-01-30 14:14:53 +0530

    first commit

---
+++ app/src/main/java/com/gowtham/videotrimmer_litr/SquareVideoView.java
@@ -0,0 +1,30 @@
+package com.gowtham.videotrimmer_litr;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+public class SquareVideoView extends VideoView {
+
+    public SquareVideoView(Context context) {
+        super(context);
+    }
+
+    public SquareVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareVideoView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        setMeasuredDimension(width, width);
+    }
+
+}
